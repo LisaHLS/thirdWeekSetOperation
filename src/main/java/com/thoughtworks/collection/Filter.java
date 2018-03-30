91c686159d58ca3bc893d0954a16b31794c28b04
LisaHuang <1449700087@qq.com>
2018-03-30 14:23:06 +0800

    提交Filter.java,求数组中的偶数

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,19 +2,23 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Filter {
 
     List<Integer>  array;
 
-    public Filter(List<Integer> array) {
-     this.array = array;
-    }
+    public Filter(List<Integer> array) {this.array = array;}
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer>  evenList = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i)%2 == 0) {
+                evenList.add(array.get(i));
+            }
+        }
+        return evenList;
     }
 
     public List<Integer> filterMultipleOfThree() {
