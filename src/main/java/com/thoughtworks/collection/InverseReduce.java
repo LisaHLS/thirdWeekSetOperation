379f01663bc49a9fd706854dac63e189ec1f961f
LisaHuang <1449700087@qq.com>
2018-03-30 15:30:20 +0800

    提交InverseReduce.java

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +15,10 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> listOfNumberDivideToSmaller = new ArrayList<>();
+        for (int i = number - random.nextInt(3); i > 0 ; i-= random.nextInt(3)) {
+            listOfNumberDivideToSmaller.add(i);
+        }
+        return listOfNumberDivideToSmaller;
     }
 }
