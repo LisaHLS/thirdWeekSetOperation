05ce159b68813891f0d2bf0bb416938a122cff34
LisaHuang <1449700087@qq.com>
2018-03-30 15:40:08 +0800

    提交MyMap.java,实现getTriple方法

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,7 +18,11 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> listOfEachElementTriple = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            listOfEachElementTriple.add(array.get(i)*3);
+        }
+        return listOfEachElementTriple;
     }
 
     public List<String> mapLetter() {
